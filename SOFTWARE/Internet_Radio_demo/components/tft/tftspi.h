c63eb8205629508a767ec1a4dc3b3150145d86a6
Chris Boudacoff <d3v1c3nv11@users.noreply.github.com>
2019-08-01 08:55:49 +0300

    added mod-lcd2.8rtp config

--- SOFTWARE/Internet_Radio_demo/components/tft/tftspi.h
+++ SOFTWARE/Internet_Radio_demo/components/tft/tftspi.h
@@ -125,6 +125,34 @@
 #define PIN_BCKL_ON  1  	// GPIO value for backlight ON
 #define PIN_BCKL_OFF 0  	// GPIO value for backlight OFF
 // ---------------------------------------------------------
+#elif CONFIG_EXAMPLE_DISPLAY_TYPE == 4
+
+// ** Set the correct configuration for Olimex's MOD-LCD2.8RTP
+// ---------------------------------------------------------
+#define DEFAULT_DISP_TYPE   DISP_TYPE_ILI9341
+#define DEFAULT_TFT_DISPLAY_WIDTH   240
+#define DEFAULT_TFT_DISPLAY_HEIGHT  320
+#define DISP_COLOR_BITS_24          0x66
+#define DEFAULT_GAMMA_CURVE         0
+#define DEFAULT_SPI_CLOCK           26000000
+#define TFT_INVERT_ROTATION         0
+#define TFT_INVERT_ROTATION1        0
+#define TFT_RGB_BGR                 0x08
+
+#define USE_TOUCH                   TOUCH_TYPE_STMPE610
+#define STMPE610_ADDR 0x9a
+
+#define PIN_NUM_MISO 4		// SPI MISO
+#define PIN_NUM_MOSI 2		// SPI MOSI
+#define PIN_NUM_CLK  14		// SPI CLOCK pin
+#define PIN_NUM_CS   21		// Display CS pin
+#define PIN_NUM_DC   15		// Display command/data pin
+#define PIN_NUM_TCS  0		// Touch screen CS pin (NOT used if USE_TOUCH=0)
+
+#define PIN_NUM_RST  12  	// GPIO used for RESET control (#16)
+#define PIN_NUM_BCKL 12  	// GPIO used for backlight control
+#define PIN_BCKL_ON  0  	// GPIO value for backlight ON
+#define PIN_BCKL_OFF 1  	// GPIO value for backlight OFF
 
 #else
 
